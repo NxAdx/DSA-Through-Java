269f7e02d133dee9bb8c22b82e3da19bbd9bc15a
Aadarsh Lokhande <lokhandeaadarsh28@gmail.com>
2023-11-04 13:59:19 +0530

    Update paT.java

--- paT.java
+++ paT.java
@@ -8,6 +8,9 @@ class paT {
         // pat6(5);
         // pat7(5);
         // pat8(5);
+        //pat09(5);
+        // pat9(5);
+        pat10(5);
     }
 
     static void pat1(int n) {
@@ -155,7 +158,7 @@ class paT {
             System.out.println();
         }
     }
-      static void pat9(int n) {
+      static void pat09(int n) {
 // *
 // **
 // ***
@@ -172,6 +175,60 @@ class paT {
                 System.out.print("*");
             }
             System.out.println();
+        }
+    }
+    static void pat9(int n) {
+    // //      * 
+    // //     * * 
+    // //    * * * 
+    // //   * * * * 
+    // //  * * * * * 
+    // //   * * * * 
+    // //    * * * 
+    // //     * * 
+    // //      * 
+
+        for (int row = 1; row <= n * 2; row++) {
+            int till = row > n ? n * 2 - row : row;
+            int space = n - till;
+            for (int sp = 1; sp <= space; sp++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= till; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pat10(int n) {
+    // //      * 
+    // //     * * 
+    // //    * * * 
+    // //   * * * * 
+    // //  * * * * * 
+    // //   * * * * 
+    // //    * * * 
+    // //     * * 
+    // //      * 
+          
+        for (int row = 1; row < n * 2; row++) {
+            if (row <= n) {
+                for (int space = n - row; space >= 1; space--) {
+                    System.out.print(" ");
+                }
+                for (int col = 1; col <= row; col++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for (int space = 1; space <= row - n; space++) {
+                    System.out.print(" ");
+                }
+                for (int col = 1; col <= (n * 2) - row; col++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println("");
         }
     }
 }
