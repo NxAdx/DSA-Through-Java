b84979dd49011ea3f67b37b69a936d46116ab33e
Aadarsh Lokhande <lokhandeaadarsh28@gmail.com>
2025-02-21 09:26:08 +0000

    new change

--- Feb 2025/anagram.java
+++ Feb 2025/anagram.java
@@ -1,17 +1,28 @@
-// https://neetcode.io/problems/is-anagram
+// https://leetcode.com/problems/valid-anagram/description/
 
 import java.util.Scanner;
 
 class anagram{
     public static void main(String ar[]){
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        String t = scan.nextLine();
-        if(s.length() == t.length()){
-        for(int i=0;i<s.length();i++){
-            
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter: ");
+        String s = sc.nextLine();
+        System.out.println("Enter: ");
+        String t = sc.nextLine();
+        int[] c1 = new int[26];
+        int[] c2 = new int[26];
+
+        for (int i = 0; i < s.length()-1; i++) {
+            c1[s.charAt(i) - 'a']++;
+            c2[t.charAt(i) - 'a']++;
         }
-    } 
-    else return false;
+        System.out.println("String one: ");
+        for(int x: c1){
+            System.out.print(x+ " ,");
+        }
+        System.out.println("String two: ");
+        for(int y: c2){
+            System.out.print(y + " ,");
+        }
     }
 }
